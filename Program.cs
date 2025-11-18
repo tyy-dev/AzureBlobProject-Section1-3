82059ba4b26257532732b3f274cd703bdf9ebdc9
Bora <173380240+tyy-dev@users.noreply.github.com>
2025-11-18 14:55:58 +0100

    Refactor Program.cs to use new fluent style StartupBuilder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,33 @@
 using AzureUdemy;
+using Azure.Storage.Blobs;
+using AzureUdemy.Services;
 
-WebApplication app = new Startup(WebApplication.CreateBuilder(args)).ConfigureServices().ConfigureApp();
-app.Run();
+new StartupBuilder(WebApplication.CreateBuilder(args)).ConfigureServices((IServiceCollection services, ConfigurationManager config) =>
+{
+    services.AddControllersWithViews();
+
+    services.AddSingleton(_ => new BlobServiceClient(config.GetValue<string>("BlobConnection")));
+
+    services.AddSingleton<IContainerService, ContainerService>();
+    services.AddSingleton<IBlobService, BlobService>();
+
+}).ConfigureApp((WebApplication app) =>
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseDefaultFiles();
+
+    app.UseRouting();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+}).Run();
